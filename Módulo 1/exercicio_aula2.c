cfb93adc5146aa087b785d65948273799a29559a
Sávio Miranda <saviomrocha7@gmail.com>
2025-12-15 14:12:13 +0000

    Programa sobre as variáveis e os tipos de dados.

--- Módulo 1/exercicio_aula2.c
+++ Módulo 1/exercicio_aula2.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int main()  {
+
+char nome[10] = "Savio";
+int idade = 24;
+float altura = 1.70;
+
+printf("Meu nome é %s\n", nome);
+printf("Eu tenho %d anos\n", idade);
+printf("Minha altura é %.2f\n", altura);
+
+return 0;
+
+}
