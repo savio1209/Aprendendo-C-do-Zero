64892c7862015460bff8fb9fe100588d0e04671a
Sávio Miranda <saviomrocha7@gmail.com>
2026-01-08 13:51:07 +0000

    adição das estruturas de repetição while e do-while.

--- Módulo 1/exercicio_aula6.c
+++ Módulo 1/exercicio_aula6.c
@@ -5,9 +5,29 @@ int main(){
 int i;
 
 printf("\nFOR:\n");
+
 for (int i = 1; i <= 10; i++){
     printf("%d\n", i);
 }
+
+printf("\nWHILE:\n");
+
+i = 10;
+
+while (i >= 1){
+printf("%d\n", i);
+i--;
+}
+
+printf("\nDO-WHILE:\n");
+
+int opcao;
+
+do {
+    printf("Digite 1 para seguir.\n");
+    printf("Digite 0 para encerrar.\n");
+    scanf("%d", &opcao);
+} while (opcao != 0);
 
 return 0;
 
